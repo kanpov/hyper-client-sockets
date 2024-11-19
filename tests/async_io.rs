c7dc83e1cdd7832756eb2ca38f8249540f98b68e
kanpov <karpovanton729@gmail.com>
2024-11-19 11:06:55 +0300

    Refactor test architecture to run emulated socket servers on a separate OS thread with a tokio runtime
    Signed-off-by: kanpov <karpovanton729@gmail.com>

--- tests/async_io.rs
+++ tests/async_io.rs
@@ -1,1 +1,41 @@
+use std::{future::Future, sync::Arc};
 
+use async_executor::Executor;
+use async_io::block_on;
+use bytes::Bytes;
+use common::{assert_response_ok, start_unix_server};
+use http::{Request, Uri};
+use http_body_util::Full;
+use hyper_client_sockets::{
+    unix::{connector::HyperUnixConnector, UnixUriExt},
+    Backend,
+};
+use hyper_util::client::legacy::Client;
+use smol_hyper::rt::SmolExecutor;
+
+mod common;
+
+#[test]
+fn unix_connectivity_with_hyper_util() {
+    run_test(|executor| async {
+        let path = start_unix_server();
+        let client: Client<_, Full<Bytes>> =
+            Client::builder(SmolExecutor::new(executor)).build(HyperUnixConnector::new(Backend::AsyncIo));
+        let request = Request::builder()
+            .uri(Uri::unix(path, "/").unwrap())
+            .method("GET")
+            .body(Full::new(Bytes::new()))
+            .unwrap();
+        let mut response = client.request(request).await.unwrap();
+        assert_response_ok(&mut response).await;
+    });
+}
+
+fn run_test<F, Fut>(function: F)
+where
+    F: FnOnce(Arc<Executor<'static>>) -> Fut,
+    Fut: Future<Output = ()>,
+{
+    let executor = Arc::new(Executor::new());
+    block_on(executor.clone().run(function(executor)));
+}
--- tests/common.rs
+++ tests/common.rs
@@ -37,42 +37,47 @@ pub fn start_unix_server() -> PathBuf {
         std::fs::remove_file(&path).unwrap();
     }
 
-    let listener = UnixListener::bind(&path).unwrap();
+    let cloned_path = path.clone();
 
-    in_tokio(async move {
-        loop {
-            let (stream, _) = listener.accept().await.unwrap();
-            let tokio_io = TokioIo::new(stream);
+    in_dedicated_thread(
+        move || UnixListener::bind(&cloned_path).unwrap(),
+        move |listener| async move {
+            loop {
+                let (stream, _) = listener.accept().await.unwrap();
+                let tokio_io = TokioIo::new(stream);
 
-            tokio::task::spawn(async move {
-                if let Err(err) = http1::Builder::new()
-                    .serve_connection(tokio_io, service_fn(hello_world_route))
-                    .await
-                {
-                    eprintln!("Error serving connection: {:?}", err);
-                }
-            });
-        }
-    });
+                tokio::task::spawn(async move {
+                    if let Err(err) = http1::Builder::new()
+                        .serve_connection(tokio_io, service_fn(hello_world_route))
+                        .await
+                    {
+                        eprintln!("Error serving connection: {:?}", err);
+                    }
+                });
+            }
+        },
+    );
 
     path
 }
 
 pub fn start_vsock_server() -> (u32, u32) {
     let port = rand::thread_rng().gen_range(10000..=65536) as u32;
-    let mut listener = VsockListener::bind(VsockAddr::new(VMADDR_CID_LOCAL, port)).unwrap();
 
-    in_tokio(async move {
-        loop {
-            let tokio_io = TokioIo::new(listener.accept().await.unwrap().0);
-            tokio::task::spawn(async move {
-                http1::Builder::new()
-                    .serve_connection(tokio_io, service_fn(hello_world_route))
-                    .await
-                    .unwrap();
-            });
-        }
-    });
+    in_dedicated_thread(
+        move || VsockListener::bind(VsockAddr::new(VMADDR_CID_LOCAL, port)).unwrap(),
+        |mut listener| async move {
+            loop {
+                let tokio_io = TokioIo::new(listener.accept().await.unwrap().0);
+                tokio::task::spawn(async move {
+                    http1::Builder::new()
+                        .serve_connection(tokio_io, service_fn(hello_world_route))
+                        .await
+                        .unwrap();
+                });
+            }
+        },
+    );
 
     (VMADDR_CID_LOCAL, port)
 }
@@ -82,45 +87,62 @@ pub fn start_firecracker_server() -> (PathBuf, u32) {
     if path.exists() {
         std::fs::remove_file(&path).unwrap();
     }
+    let cloned_path = path.clone();
+
     let guest_port = rand::thread_rng().gen_range(1..=1000) as u32;
 
-    let listener = UnixListener::bind(&path).unwrap();
+    in_dedicated_thread(
+        move || UnixListener::bind(&cloned_path).unwrap(),
+        move |listener| async move {
+            loop {
+                // Recreate the CONNECT behavior of a real Firecracker socket
+                let (mut stream, _) = listener.accept().await.unwrap();
+                let mut buf_reader = BufReader::new(&mut stream).lines();
+                let mut line = String::new();
+                buf_reader.get_mut().read_line(&mut line).await.unwrap();
 
-    in_tokio(async move {
-        loop {
-            // Recreate the CONNECT behavior of a real Firecracker socket
-            let (mut stream, _) = listener.accept().await.unwrap();
-            let mut buf_reader = BufReader::new(&mut stream).lines();
-            let mut line = String::new();
-            buf_reader.get_mut().read_line(&mut line).await.unwrap();
+                if line == format!("CONNECT {guest_port}\n") {
+                    stream.write_all(b"OK\n").await.unwrap();
+                } else {
+                    stream.write_all(b"REJECTED\n").await.unwrap();
+                    return;
+                }
 
-            if line == format!("CONNECT {guest_port}\n") {
-                stream.write_all(b"OK\n").await.unwrap();
-            } else {
-                stream.write_all(b"REJECTED\n").await.unwrap();
-                return;
+                // After sending out approval, serve HTTP
+                let tokio_io = TokioIo::new(stream);
+                tokio::task::spawn(async move {
+                    http1::Builder::new()
+                        .serve_connection(tokio_io, service_fn(hello_world_route))
+                        .await
+                        .unwrap();
+                });
             }
-
-            // After sending out approval, serve HTTP
-            let tokio_io = TokioIo::new(stream);
-            tokio::task::spawn(async move {
-                http1::Builder::new()
-                    .serve_connection(tokio_io, service_fn(hello_world_route))
-                    .await
-                    .unwrap();
-            });
-        }
-    });
+        },
+    );
 
     (path, guest_port)
 }
 
-fn in_tokio(future: impl Future<Output = ()> + Send + 'static) {
+fn in_dedicated_thread<Listener, MakeListener, Act, ActFuture>(make_listener: MakeListener, act: Act)
+where
+    MakeListener: 'static + Send + FnOnce() -> Listener,
+    Listener: Send,
+    Act: 'static + Send + FnOnce(Listener) -> ActFuture,
+    ActFuture: Future<Output = ()> + Send,
+{
+    let (tx, rx) = std::sync::mpsc::channel();
+
     std::thread::spawn(move || {
         tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
             .unwrap()
-            .block_on(future)
+            .block_on(async move {
+                let listener = make_listener();
+                tx.send(()).unwrap();
+                act(listener).await;
+            })
     });
+
+    rx.recv().unwrap();
 }
